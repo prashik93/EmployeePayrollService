ad97b48c302575d529bec86ec15c787aa399c960
Prashik Kamble <kambleprashik7349@gmail.com>
2022-12-28 23:23:29 +0530

    ADDED : Query to create Employee Payroll Table into Payroll_Service Database.

--- src/main/java/org/example/EmployeePayrollService.sql
+++ src/main/java/org/example/EmployeePayrollService.sql
@@ -3,3 +3,11 @@
 CREATE DATABASE payroll_service;    # To Create Database
 SHOW DATABASES;                     # To Show Databases in the MySql
 USE payroll_service;                # To Use Perticular Database
+
+# UC2_CreateEmployeeTable
+create table employee_payroll(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(45) NOT NULL,
+    salary FLOAT NOT NULL,
+    startdate DATE NOT NULL
+);

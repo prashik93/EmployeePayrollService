e39dccec9b813ed7abeb3d44deeeda7e20f576b0
Prashik Kamble <kambleprashik7349@gmail.com>
2022-12-28 23:04:30 +0530

    ADDED : Query to create database, show database and use database.

---
+++ src/main/java/org/example/EmployeePayrollService.sql
@@ -0,0 +1,5 @@
+# UC1_CreatePayrollServiceDatabase
+
+CREATE DATABASE payroll_service;    # To Create Database
+SHOW DATABASES;                     # To Show Databases in the MySql
+USE payroll_service;                # To Use Perticular Database
